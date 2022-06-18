d15bce1b74ca8588635bbcb070594b954db5917e
NathanKell <nathankell@gmail.com>
2022-06-18 12:10:42 -0700

    Prep for 2.0

--- source/KerKonConConExt/Properties/AssemblyInfo.cs
+++ source/KerKonConConExt/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.30.5")]
-[assembly: AssemblyInformationalVersion("1.30.5")]
-[assembly: KSPAssembly("KerKonConConExt", 1, 0)]
-[assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: KSPAssembly("KerKonConConExt", 2, 0)]
+[assembly: KSPAssemblyDependency("ContractConfigurator", 2, 0)]
 [assembly: KSPAssemblyDependency("KerbalKonstructs", 0, 9)]
